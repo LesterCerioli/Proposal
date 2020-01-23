5077874196f0bb6fa753057dbbe4ea40364aedb5
lester.cerioli <lester.cerioli@blendit.com.br>
2020-01-23 15:59:10 -0300

    Data model scripts

--- Proposal.Web/ClientApp/src/app/app.module.ts
+++ Proposal.Web/ClientApp/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-@NgModule({
+import {HomeComponent} from "./home/home.component";@NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' }
+      
+    ])
+    
+      
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ Proposal.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'app-nav-menu',
+    templateUrl: './nav-menu.component.html',
+    styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent implements OnInit {    
+    
+
+    
+}
