cf36c4219fdca418b1759bbde2aa7136a27f59bf
lester.cerioli <lester.cerioli@blendit.com.br>
2020-01-24 12:40:46 -0300

    Update

--- Proposal.Web/ClientApp/src/app/app.module.ts
+++ Proposal.Web/ClientApp/src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HomeComponent} from "./home/home.component";@NgModule({
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { CustomersComponent } from './customers/customers.component';
+
+@NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    NavMenuComponent,
+    HomeComponent,
+    ResourcesComponent,
+    CustomersComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
-      
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'resources', component: ResourcesComponent },
+      { path: 'customers', component: CustomersComponent },
     ])
-    
-      
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
---
+++ Proposal.Web/ClientApp/src/app/resources/resources.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-resources',
+    template: "./resources.component.html",
+})
+
+export class ResourcesComponent {
+    
+}
